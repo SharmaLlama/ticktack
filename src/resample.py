252b239f51f7abbcd19cee983d12f8452fdd300c
qingyuanzhang3 <74750938+qingyuanzhang3@users.noreply.github.com>
2022-03-07 10:05:14 +1000

    mc inverse solver

--- src/resample.py
+++ src/resample.py
@@ -12,12 +12,12 @@ warnings.filterwarnings("ignore")
 
 class Resampler:
 
-    def load_data(self, time_data, d14c_data, d14c_data_error):
-        self.time_data = time_data
-        self.d14c_data = d14c_data
-        self.d14c_data_error = d14c_data_error
-        self.start = np.nanmin(self.time_data)
-        self.end = np.nanmax(self.time_data)
+    def load_data(self, x, y, yerr):
+        self.x = x
+        self.y = y
+        self.yerr = yerr
+        self.start = np.nanmin(self.x)
+        self.end = np.nanmax(self.x)
         self.annual = jnp.arange(self.start, self.end + 1)
 
     def super_gaussian(self, x, sz, order=30):
@@ -30,13 +30,12 @@ class Resampler:
         freq = fftfreq(n, d=1)
         mask = self.super_gaussian(np.abs(freq) - l, sz, order=order).reshape(1, -1)
         new_signal = ifft(fft(signal) * mask)
-        if new_signal.shape[0] == 1:
-            return np.squeeze(new_signal)
-        return new_signal
+        return np.real(np.squeeze(new_signal))
 
-    def signal_resample(self, size=10000, l=1./9.677, sz=0.05, order=30):
-        samples = np.random.multivariate_normal(self.d14c_data, np.diag(self.d14c_data_error ** 2), size=size)
-        f = interp1d(self.time_data, samples, kind="cubic")
+    def signal_resample(self, size=10000, l=1./9.677, sz=0.05, order=30, samples=None):
+        if not np.all(samples):
+            samples = np.random.multivariate_normal(self.y, np.diag(self.yerr ** 2), size=size)
+        f = interp1d(self.x, samples, kind="cubic")
         interpolation = f(self.annual)
         new_sig = self.bandpass_super_gaussian(interpolation, l, sz, order=order)
         return new_sig
