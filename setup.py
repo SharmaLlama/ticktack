8db0bc5014f99ea7359b06cc3eaf298f1090565c
Benjamin Pope <b.pope@uq.edu.au>
2023-02-12 15:18:07 +1000

    remove numpy from install

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-from numpy.distutils.core import setup, Extension
-from numpy.distutils.misc_util import Configuration
-import distutils.sysconfig as ds
 from setuptools import setup
 import os
 import codecs
